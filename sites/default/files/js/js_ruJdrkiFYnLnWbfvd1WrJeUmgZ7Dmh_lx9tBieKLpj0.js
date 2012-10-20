8f896aa0e53acbf2b19d5bf47f4aae65f9ddcff5
Pirate <pir@server>
2012-10-20 17:44:59 +0000

    many added files

---
+++ sites/default/files/js/js_ruJdrkiFYnLnWbfvd1WrJeUmgZ7Dmh_lx9tBieKLpj0.js
@@ -0,0 +1,180 @@
+(function ($) {
+
+Drupal.behaviors.textarea = {
+  attach: function (context, settings) {
+    $('.form-textarea-wrapper.resizable', context).once('textarea', function () {
+      var staticOffset = null;
+      var textarea = $(this).addClass('resizable-textarea').find('textarea');
+      var grippie = $('<div class="grippie"></div>').mousedown(startDrag);
+
+      grippie.insertAfter(textarea);
+
+      function startDrag(e) {
+        staticOffset = textarea.height() - e.pageY;
+        textarea.css('opacity', 0.25);
+        $(document).mousemove(performDrag).mouseup(endDrag);
+        return false;
+      }
+
+      function performDrag(e) {
+        textarea.height(Math.max(32, staticOffset + e.pageY) + 'px');
+        return false;
+      }
+
+      function endDrag(e) {
+        $(document).unbind('mousemove', performDrag).unbind('mouseup', endDrag);
+        textarea.css('opacity', 1);
+      }
+    });
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+
+/**
+ * Open links to Mollom.com in a new window.
+ *
+ * Required for valid XHTML Strict markup.
+ */
+Drupal.behaviors.mollomTarget = {
+  attach: function (context) {
+    $(context).find('.mollom-target').click(function () {
+      this.target = '_blank';
+    });
+  }
+};
+
+/**
+ * Attach click event handlers for CAPTCHA links.
+ */
+Drupal.behaviors.mollomCaptcha = {
+  attach: function (context, settings) {
+    // @todo Pass the local settings we get from Drupal.attachBehaviors(), or
+    //   inline the click event handlers, or turn them into methods of this
+    //   behavior object.
+    $('a.mollom-switch-captcha', context).click(getMollomCaptcha);
+  }
+};
+
+/**
+ * Fetch a Mollom CAPTCHA and output the image or audio into the form.
+ */
+function getMollomCaptcha() {
+  // Get the current requested CAPTCHA type from the clicked link.
+  var newCaptchaType = $(this).hasClass('mollom-audio-captcha') ? 'audio' : 'image';
+
+  var context = $(this).parents('form');
+
+  // Extract the form build ID and Mollom content ID from the form.
+  var formBuildId = $('input[name="form_build_id"]', context).val();
+  var mollomContentId = $('input.mollom-content-id', context).val();
+
+  var path = 'mollom/captcha/' + newCaptchaType + '/' + formBuildId;
+  if (mollomContentId) {
+    path += '/' + mollomContentId;
+  }
+
+  // Retrieve a CAPTCHA:
+  $.getJSON(Drupal.settings.basePath + path,
+    function (data) {
+      if (!(data && data.content)) {
+        return;
+      }
+      // Inject new CAPTCHA.
+      $('.mollom-captcha-content', context).parent().html(data.content);
+      // Update CAPTCHA ID.
+      $('input.mollom-captcha-id', context).val(data.captchaId);
+      // Add an onclick-event handler for the new link.
+      Drupal.attachBehaviors(context);
+      // Focus on the CAPTCHA input.
+      $('input[name="mollom[captcha]"]', context).focus();
+    }
+  );
+  return false;
+}
+
+})(jQuery);
+;
+
+/**
+ * JavaScript behaviors for the front-end display of webforms.
+ */
+
+(function ($) {
+
+Drupal.behaviors.webform = Drupal.behaviors.webform || {};
+
+Drupal.behaviors.webform.attach = function(context) {
+  // Calendar datepicker behavior.
+  Drupal.webform.datepicker(context);
+};
+
+Drupal.webform = Drupal.webform || {};
+
+Drupal.webform.datepicker = function(context) {
+  $('div.webform-datepicker').each(function() {
+    var $webformDatepicker = $(this);
+    var $calendar = $webformDatepicker.find('input.webform-calendar');
+    var startDate = $calendar[0].className.replace(/.*webform-calendar-start-(\d{4}-\d{2}-\d{2}).*/, '$1').split('-');
+    var endDate = $calendar[0].className.replace(/.*webform-calendar-end-(\d{4}-\d{2}-\d{2}).*/, '$1').split('-');
+    var firstDay = $calendar[0].className.replace(/.*webform-calendar-day-(\d).*/, '$1');
+    // Convert date strings into actual Date objects.
+    startDate = new Date(startDate[0], startDate[1] - 1, startDate[2]);
+    endDate = new Date(endDate[0], endDate[1] - 1, endDate[2]);
+
+    // Ensure that start comes before end for datepicker.
+    if (startDate > endDate) {
+      var laterDate = startDate;
+      startDate = endDate;
+      endDate = laterDate;
+    }
+
+    var startYear = startDate.getFullYear();
+    var endYear = endDate.getFullYear();
+
+    // Set up the jQuery datepicker element.
+    $calendar.datepicker({
+      dateFormat: 'yy-mm-dd',
+      yearRange: startYear + ':' + endYear,
+      firstDay: parseInt(firstDay),
+      minDate: startDate,
+      maxDate: endDate,
+      onSelect: function(dateText, inst) {
+        var date = dateText.split('-');
+        $webformDatepicker.find('select.year, input.year').val(+date[0]);
+        $webformDatepicker.find('select.month').val(+date[1]);
+        $webformDatepicker.find('select.day').val(+date[2]);
+      },
+      beforeShow: function(input, inst) {
+        // Get the select list values.
+        var year = $webformDatepicker.find('select.year, input.year').val();
+        var month = $webformDatepicker.find('select.month').val();
+        var day = $webformDatepicker.find('select.day').val();
+
+        // If empty, default to the current year/month/day in the popup.
+        var today = new Date();
+        year = year ? year : today.getFullYear();
+        month = month ? month : today.getMonth() + 1;
+        day = day ? day : today.getDate();
+
+        // Make sure that the default year fits in the available options.
+        year = (year < startYear || year > endYear) ? startYear : year;
+
+        // jQuery UI Datepicker will read the input field and base its date off
+        // of that, even though in our case the input field is a button.
+        $(input).val(year + '-' + month + '-' + day);
+      }
+    });
+
+    // Prevent the calendar button from submitting the form.
+    $calendar.click(function(event) {
+      $(this).focus();
+      event.preventDefault();
+    });
+  });
+}
+
+})(jQuery);
+;
